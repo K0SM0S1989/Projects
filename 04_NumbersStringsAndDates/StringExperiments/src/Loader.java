b29136b194dfdf6fe9fc2f70d7e34388aec087fe
Mikhail Artamokhin <misharta@yandex.ru>
2020-02-08 12:56:06 +0300

    4.4 Home Work  Salary Vasya and Petya and Masha

--- 04_NumbersStringsAndDates/StringExperiments/src/Loader.java
+++ 04_NumbersStringsAndDates/StringExperiments/src/Loader.java
@@ -5,6 +5,20 @@ public class Loader
     {
         String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
 
-        System.out.println(text);
+        String vasyaString = text.substring(text.indexOf('л')+1).trim();
+        String vasyaSalary = vasyaString.substring(0, vasyaString.indexOf(' '));
+
+        String petyaString = text.substring(text.indexOf('-')+1).trim();
+        String petyaSalary = petyaString.substring(0, petyaString.indexOf(' '));
+
+        String mashaString = text.substring(text.lastIndexOf('-')+1).trim();
+        String mashaSalary = mashaString.substring(0, mashaString.indexOf(' '));
+
+        int sumSalaryAllFriends = Integer.parseInt(vasyaSalary)+Integer.parseInt(petyaSalary)+Integer.parseInt(mashaSalary);
+        int sumSalryVasyaMasha = Integer.parseInt(vasyaSalary)+Integer.parseInt(mashaSalary);
+
+        System.out.println("Сумма заработка всех друзей = "+sumSalaryAllFriends);
+        System.out.println("Сумма заработка Васи и Маши = "+sumSalryVasyaMasha);
+
     }
 }
