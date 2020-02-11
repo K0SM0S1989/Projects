7cabfb26040426b82def24759d77a0f779df0cdf
Mikhail Artamokhin <misharta@yandex.ru>
2020-02-11 21:00:59 +0300

    create metodes averageTemperatures() and healthPacients()

--- 05_ArraysAndCollections/5.1 Hospital/src/Main.java
+++ 05_ArraysAndCollections/5.1 Hospital/src/Main.java
@@ -3,23 +3,41 @@ public class Main {
     public static void main(String[] args) {
 
         double [] temperature = new double[30];
-        double sumTemp = 0;
-        int healthyPatientsCount = 0;
+
         for (int i = 0; i<temperature.length;i++){
             temperature[i] = Math.random()*8+32;
             temperature[i] = roundAvoid(temperature[i], 1);
-            if (temperature[i]>=36.2 && temperature[i]<=36.9){
-                healthyPatientsCount += 1;
-            }
             System.out.printf(" Температура %d-ого больного = %.1f",(i+1), temperature[i]);
-            sumTemp += temperature[i];
         }
-        double averageTemp = sumTemp/temperature.length;
-        System.out.printf("\nСредняя температура по больнице = %.1f",averageTemp);
-        System.out.println("\nКоличество здоровых пациентов = "+ healthyPatientsCount);
+
+        averageTemperatures(temperature);
+        healthPacients(temperature);
+
     }
+
     public static double roundAvoid(double value, int places) {
         double scale = Math.pow(10, places);
         return Math.round(value * scale) / scale;
     }
+
+
+    public static void averageTemperatures(double[] allTemp){
+        double sumTemp = 0;
+        for (int i = 0; i<allTemp.length;i++){
+            sumTemp += allTemp[i];
+        }
+        double averageTemp = sumTemp/allTemp.length;
+        System.out.printf("\nСредняя температура по больнице = %.1f",averageTemp);
+    }
+
+    public static void healthPacients(double[] allTemp){
+        int healthyPatientsCount = 0;
+        for (int i = 0; i<allTemp.length;i++){
+            if (allTemp[i]>=36.2 && allTemp[i]<=36.9){
+                healthyPatientsCount += 1;
+            }
+        }
+        System.out.println("\nКоличество здоровых пациентов = "+ healthyPatientsCount);
+    }
+
 }
