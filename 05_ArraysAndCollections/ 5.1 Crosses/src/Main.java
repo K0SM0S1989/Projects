afd5cddbd1be974c994ab3e6179d1bdb9002acad
Mikhail Artamokhin <misharta@yandex.ru>
2020-02-11 23:29:42 +0300

    code fix by array

--- 05_ArraysAndCollections/ 5.1 Crosses/src/Main.java
+++ 05_ArraysAndCollections/ 5.1 Crosses/src/Main.java
@@ -1,11 +1,36 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        String [][] text = {{"x     x"},{" x   x "},{"  x x  "}, {"   x   "},{"  x x  "},{" x   x "},{"x     x"}};
-        for (int i = 0;i<text.length;i++){
-            for (int j=0;j<text[i].length;j++){
-                System.out.println(text[i][j]);
-            }
-        }
+
+        System.out.println("Для определения размера квадратной матрицы \nвведите целое число:");
+        Scanner scanner = new Scanner(System.in);
+       try {
+           int inputData = scanner.nextInt();
+
+           String space = " ";
+           String x = "x";
+
+           String [][] text = new String[inputData][inputData];
+           int k = text.length-1;
+           for (int i = 0; i<text.length; i++){
+               text[i][i]=x;
+               text[k][i]=x;
+               k--;
+           }
+           for (int i = 0;i<text.length;i++){
+               for (int j=0;j<text[i].length;j++){
+                   if (text[i][j]==null){
+                       text[i][j]=space;
+                   }
+                   System.out.print(text[i][j]+"\t");
+               }
+               System.out.println();
+           }
+       }catch (Exception e){
+           System.out.println("Введен неверный символ");
+       }
+
     }
 }
