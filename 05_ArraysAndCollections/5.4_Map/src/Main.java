3a5cf6b30fbc95e50c5720be42cd895988dda752
Mikhail Artamokhin <misharta@yandex.ru>
2020-02-17 00:34:40 +0300

    5.4 fix problems TreeMap

--- 05_ArraysAndCollections/5.4_Map/src/Main.java
+++ 05_ArraysAndCollections/5.4_Map/src/Main.java
@@ -4,20 +4,21 @@ import java.util.TreeMap;
 
 public class Main {
 
-    static String phoneNumber = "[0-9]+";
-    static String name = "[А-Яа-я]+";
+   private static final String PHONE_NUMBER = "[0-9]+";
+   private static final String NAME = "[А-Яа-я]+";
     static TreeMap<String, String> phoneBook;
 
     public static void main(String[] args) {
         phoneBook = new TreeMap<String, String>();
 
         Scanner scanner = new Scanner(System.in);
-        for (;;){
+
+        while (true){
             String phoneBookItem = scanner.nextLine().trim();
             String [] wordsOfInput = phoneBookItem.split("\\s+");
             if (wordsOfInput.length > 1){
                 if (isValidName(wordsOfInput[0]) && isValidPhoneNumber(wordsOfInput[1])){
-                    phoneBook.put(wordsOfInput[1], wordsOfInput[0]);
+                    phoneBook.put(wordsOfInput[0], wordsOfInput[1]);
                 } else {
                     System.out.println("Ошибка ввода! \nПопробуйте снова");
                 }
@@ -39,64 +40,51 @@ public class Main {
     }
     private static void printMap(Map<String,String> map){
         for (String key : map.keySet()){
-            System.out.println(map.get(key)+ " - "+ key);
+            System.out.println(key+ " - "+ map.get(key));
         }
     }
 
     private static boolean isValidPhoneNumber(String word){
-        boolean check = false;
-        if (word.matches(phoneNumber)){
-            check = true;
-        }
-        return check;
+        return word.matches(PHONE_NUMBER);
     }
 
     private static boolean isValidName(String word){
-        boolean check = false;
-        if (word.matches(name)){
-            check = true;
-        }
-        return check;
+        return word.matches(NAME);
     }
 
     private  static void checkKeyValue (String word, Map<String,String> map){
-        boolean check = false;
-        for (String key : map.keySet()){
-            if (word.matches(key) || word.matches(map.get(key))){
-                if (isValidPhoneNumber(word)){
-                    System.out.println("Такой номер есть в списке:");
-                    System.out.println(map.get(key)+ " - "+ key);
-                }
-                if (isValidName(word)){
-                    System.out.println("Такое имя есть в списке:");
-                    System.out.println(map.get(key)+ " - "+ key);
-                }
 
-                check = true;
-                break;
-            }
-
-        }
-        if (!check){
+        if (!map.containsKey(word) && !map.containsValue(word) ){
             if (isValidPhoneNumber(word)){
                 System.out.println("Совпадений нет! \nДобавьте Имя");
-                addData(word, map);
+                Scanner scanner = new Scanner(System.in);
+
+                addData(word, map, scanner.nextLine().trim());
             }
             if (isValidName(word)){
                 System.out.println("Совпадений нет! \nДобавьте номер");
-                addData(word, map);
+                Scanner scanner = new Scanner(System.in);
+                addData(word, map, scanner.nextLine().trim());
             }
 
-        }
+        } else {System.out.println("Данные есть в списке:");
+            for (Map.Entry<String,String> entry : map.entrySet()) {
+                if (word.equals(entry.getValue())) {
+                    System.out.println(entry.getKey() + " - "+word);
+                }
+            }
+                if (map.containsKey(word)){
+                        System.out.println(word + " - "+map.get(word));
+                    }
+            }
 
     }
-    private static void addData(String word, Map<String,String> map){
-        Scanner scanner = new Scanner(System.in);
-        String newData = scanner.nextLine().trim();
+    private static void addData(String word, Map<String,String> map, String newData){
+
         if (isValidPhoneNumber(word)){
-            map.put(word, newData);
+            map.put(newData,word);
         }else if (isValidName(word)){
-            map.put(newData, word);
+            map.put(word, newData);
         }
     }
 
