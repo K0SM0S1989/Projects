d39e260e716d2a515cd5b03beb4a92af7e47cd9d
Mikhail Artamokhin <misharta@yandex.ru>
2020-03-11 18:36:33 +0300

    9.1 Home work Fix Problems

--- 09_FilesAndNetwork/9.1_Files/src/Main.java
+++ 09_FilesAndNetwork/9.1_Files/src/Main.java
@@ -9,21 +9,26 @@ public class Main {
     public static void main(String[] args) {
 
        try {
-           Stream<Path> pathStream = Files.walk(Paths.get("C:\\Users\\Kosmos\\Desktop\\new"));
-           int kilo = 1024;
+           Stream<Path> pathStream = Files.walk(Paths.get(args[0]));
+
            long sum = pathStream.filter(Files::isRegularFile).map(Path::toFile).map(File::length).mapToLong(Long::longValue).sum();
-           if (sum >= kilo && sum <=Math.pow(kilo,2)){
-               System.out.println( (int)(sum/kilo) + " кб");
-           }else if (sum > Math.pow(kilo,2) && sum <=Math.pow(kilo,3)){
-               System.out.println(String.format("%.1f",sum/Math.pow(kilo,2))  + " Мб");
-           }else if (sum > Math.pow(kilo,3) && sum <=Math.pow(kilo,4)){
-               System.out.println( String.format("%.1f",sum/Math.pow(kilo,3))  + " Гб");
-           }
-           else System.out.println(sum+" байт");
+           weight(sum);
 
        }catch (Exception e) {
            e.printStackTrace();
        }
 
+    }
+
+    private static void weight(long sum){
+        int kilo = 1024;
+        if (sum >= kilo && sum <=Math.pow(kilo,2)){
+            System.out.println( (int)(sum/kilo) + " кб");
+        }else if (sum > Math.pow(kilo,2) && sum <=Math.pow(kilo,3)){
+            System.out.println(String.format("%.1f",sum/Math.pow(kilo,2))  + " Мб");
+        }else if (sum > Math.pow(kilo,3) && sum <=Math.pow(kilo,4)){
+            System.out.println( String.format("%.1f",sum/Math.pow(kilo,3))  + " Гб");
+        }
+        else System.out.println(sum+" байт");
     }
 }
