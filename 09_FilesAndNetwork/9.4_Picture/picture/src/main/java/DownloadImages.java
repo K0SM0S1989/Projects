0a926e3ffe6ed6f7cbbbeab59d6832fe35be3e91
Mikhail Artamokhin <misharta@yandex.ru>
2020-03-22 11:42:40 +0300

    9.4 home work fix problems

--- 09_FilesAndNetwork/9.4_Picture/picture/src/main/java/DownloadImages.java
+++ 09_FilesAndNetwork/9.4_Picture/picture/src/main/java/DownloadImages.java
@@ -1,13 +1,16 @@
 import java.io.*;
 import java.net.URL;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
 import java.io.IOException;
 
 public class DownloadImages {
-    private static  String folderPath = "ForImage";
+    private static String folderPath = "ForImage";
+
     public static void main(String[] args) {
         try {
             Document doc = Jsoup.connect("https://lenta.ru").get();
@@ -23,19 +26,20 @@ public class DownloadImages {
             System.err.println("There was an error");
         }
     }
-    private static void getImages(String src)throws IOException {
+
+    private static void getImages(String src) throws IOException {
         String wrangSymbol = "[\\:*?\"<>|]";
         int indexname = src.lastIndexOf("/");
         String nameAll = src.substring(indexname, src.length());
-        String name = nameAll.replaceAll(wrangSymbol,"_");
+        String name = nameAll.replaceAll(wrangSymbol, "_");
         System.out.println(name);
-                    URL url = new URL(src);
-            InputStream in = url.openStream();
-            OutputStream out = new BufferedOutputStream(new FileOutputStream(folderPath + name));
-        for (int b = 0; (b = in.read()) != -1; ) {
-            out.write(b);
+        URL url = new URL(src);
+        InputStream in = url.openStream();
+        try (OutputStream out = new BufferedOutputStream(new FileOutputStream(folderPath + name))) {
+            for (int b = 0; (b = in.read()) != -1; ) {
+                out.write(b);
+            }
         }
-            out.close();
-            in.close();
+        in.close();
     }
 }
