166a9f603a4a447c8ed2753fc3154431b68ebcaf
Mikhail <misharta@yandex.ru>
2020-04-16 12:31:14 +0300

    10.1 home work correct month count

--- 10_SQLAndHibernate/10.1/JDBCExperiments/src/main/java/Main.java
+++ 10_SQLAndHibernate/10.1/JDBCExperiments/src/main/java/Main.java
@@ -7,9 +7,9 @@ public class Main {
         String pass = "";//Пароль не хотелось бы светить ибо он у меня много где используется
 
         String sqlCode = "select course_name as name, " +
-                "count(student_name)/timestampdiff(month," +
+                "count(student_name)/(timestampdiff(day," +
                 "(select subscription_date from Purchaselist order by subscription_date limit 1), " +
-                "(select subscription_date from Purchaselist order by subscription_date desc limit 1))" +
+                "(select subscription_date from Purchaselist order by subscription_date desc limit 1))/30)" +
                 " as average_per_month from Purchaselist group by course_name";
         try (Connection connection = DriverManager.getConnection(url, user, pass);
              Statement statement = connection.createStatement();
