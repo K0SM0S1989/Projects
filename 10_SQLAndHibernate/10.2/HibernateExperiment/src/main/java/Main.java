d5d461e98745cb052da2bd0cbbf3b9a3d2e88816
Mikhail <misharta@yandex.ru>
2020-04-16 17:15:16 +0300

    10.2 home work Create project HibernateExperiment

---
+++ 10_SQLAndHibernate/10.2/HibernateExperiment/src/main/java/Main.java
@@ -0,0 +1,32 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class Main {
+    public static void main(String[] args) {
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+        Metadata metadata = new MetadataSources(registry).getMetadataBuilder().build();
+        SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
+
+        Session session = sessionFactory.openSession();
+
+        Course course = session.get(Course.class, 3);
+        Teacher teacher = session.get(Teacher.class, course.getTeacherId());
+
+        System.out.println("Название курса - " + course.getName() + "\n" +
+                "Продолжительность курса - " + course.getDuration() + "\n" +
+                "Тип курса - " + course.getType() + "\n" +
+                "Описание курса - " + course.getDescription() + "\n" +
+                "Имя преподавателя - " + teacher.getName() + "\n" +
+                "Количество студентов на курсе - " + course.getStudentsCount() + "\n" +
+                "Стоимость курса - " + course.getPrice() + "\n" +
+                "Стоимость курса в час - " + course.getPricePerHour());
+
+        sessionFactory.close();
+
+    }
+}
+
