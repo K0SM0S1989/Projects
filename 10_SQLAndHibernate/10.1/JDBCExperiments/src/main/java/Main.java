4bb79a964c4fc406724da30e09127b0588cb746d
Mikhail <misharta@yandex.ru>
2020-04-16 22:16:16 +0300

    10.1 home work fix problems part 2

--- 10_SQLAndHibernate/10.1/JDBCExperiments/src/main/java/Main.java
+++ 10_SQLAndHibernate/10.1/JDBCExperiments/src/main/java/Main.java
@@ -2,15 +2,16 @@ import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
+
         String url = "jdbc:mysql://localhost:3306/skillbox?useUnicode=true&serverTimezone=UTC";
         String user = "root";
         String pass = "";//Пароль не хотелось бы светить ибо он у меня много где используется
 
-        String sqlCode = "select course_name as name, " +
-                "count(student_name)/(timestampdiff(day," +
-                "(select subscription_date from Purchaselist order by subscription_date limit 1), " +
-                "(select subscription_date from Purchaselist order by subscription_date desc limit 1))/30)" +
-                " as average_per_month from Purchaselist group by course_name";
+        String sqlCode = "SELECT  course_name as name,\n" +
+                "(COUNT(student_name)/TIMESTAMPDIFF(MONTH, MIN(subscription_date), \n" +
+                "MAX(subscription_date))\n" +
+                ") as average_per_month\n" +
+                "FROM PurchaseList GROUP BY course_name;";
         try (Connection connection = DriverManager.getConnection(url, user, pass);
              Statement statement = connection.createStatement();
              ResultSet resultSet = statement.executeQuery(sqlCode)) {
