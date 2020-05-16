67bdaceea1aa30bd5337e1a3a861e0e33ff0cb8b
Mikhail <misharta@yandex.ru>
2020-05-16 16:31:42 +0300

    11.2 home work implementation metods transfer and getBalance p.2 (Delet HashMap in BankIsFraudTrue.class

--- 11_Multithreading/Transactions/src/test/java/BankISFraudTrue.java
+++ 11_Multithreading/Transactions/src/test/java/BankISFraudTrue.java
@@ -1,15 +1,10 @@
-import java.util.HashMap;
 
 public class BankISFraudTrue extends Bank {
-
-   private  HashMap<String, Account> blockAccounts = new HashMap<>();
 
 
     @Override
     public synchronized boolean isFraud(String fromAccountNum, String toAccountNum, long amount) throws InterruptedException {
         return true;
     }
-
-
 
 }
