45c083908eda23134557155e83108eaac7fcc6e4
Mikhail <misharta@yandex.ru>
2020-07-04 20:29:10 +0300

    12.1 home work - fix problems part 1

---
+++ 12_SpringBootWebDevelopment/ToDoList/src/test/java/main/DefaultControllerTest.java
@@ -0,0 +1,19 @@
+package main;
+
+import org.junit.Test;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class DefaultControllerTest extends LoginTest {
+
+
+
+    @Test
+    public void shouldReturnDefaultMessage() throws Exception {
+        this.mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+}
---
+++ 12_SpringBootWebDevelopment/ToDoList/src/test/java/main/LoginTest.java
@@ -0,0 +1,40 @@
+package main;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import main.controller.DefaultController;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public abstract class LoginTest {
+
+    @Autowired
+    protected WebApplicationContext ctx;
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected DefaultController controller;
+
+    @Autowired
+    protected ObjectMapper mapper;
+
+    @Before
+    public void setup() throws Exception {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(ctx)
+                .build();
+    }
+
+}
