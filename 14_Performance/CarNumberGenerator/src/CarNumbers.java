3ba6b79656be6d6ab4615c0067c655c7139efb5c
Mikhail <misharta@yandex.ru>
2020-09-29 22:52:31 +0300

    14.1 home work, Multithreading generate car numbers

---
+++ 14_Performance/CarNumberGenerator/src/CarNumbers.java
@@ -0,0 +1,62 @@
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.concurrent.RecursiveAction;
+
+public class CarNumbers extends RecursiveAction {
+
+    int regionCode;
+    final char[] letters = {'У', 'К', 'Е', 'Н', 'Х', 'В', 'А', 'Р', 'О', 'С', 'М', 'Т'};
+
+    public CarNumbers(int regionCode) {
+
+        this.regionCode = regionCode;
+
+    }
+
+    @Override
+    protected void compute() {
+        if (regionCode < 100) {
+            CarNumbers task = new CarNumbers(createCarNumbers());
+            invokeAll(task);
+        }
+    }
+
+    int createCarNumbers() {
+        StringBuffer numberBuilder = new StringBuffer();
+        int i = regionCode;
+        StringBuffer builder = new StringBuffer();
+        try (PrintWriter writer = new PrintWriter("res/numbers" + i + ".txt")) {
+            for (int number = 1; number < 1000; number++) {
+                for (char firstLetter : letters) {
+                    for (char secondLetter : letters) {
+                        for (char thirdLetter : letters) {
+                            builder.append(firstLetter)
+                                    .append(padNumber(number, 3, numberBuilder))
+                                    .append(secondLetter)
+                                    .append(thirdLetter)
+                                    .append(padNumber(i, 2, numberBuilder))
+                                    .append('\n');
+                        }
+                    }
+                }
+
+            }
+            writer.write(builder.toString());
+            i++;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return i;
+    }
+
+
+    StringBuffer padNumber(int number, int numberLength, StringBuffer numberBuilder) {
+        numberBuilder.delete(0, numberBuilder.length());
+        numberBuilder.append(number);
+        int padSize = numberLength - numberBuilder.length();
+        for (int i = 0; i < padSize; i++) {
+            numberBuilder.insert(0, 0);
+        }
+        return numberBuilder;
+    }
+}
--- 14_Performance/CarNumberGenerator/src/Loader.java
+++ 14_Performance/CarNumberGenerator/src/Loader.java
@@ -1,45 +1,22 @@
-import java.io.FileOutputStream;
+import java.util.concurrent.ForkJoinPool;
 
-public class Loader
-{
-    public static void main(String[] args) throws Exception
-    {
+
+public class Loader {
+
+
+    public static void main(String[] args) throws Exception {
         long start = System.currentTimeMillis();
 
-        FileOutputStream writer = new FileOutputStream("res/numbers.txt");
+        //FileOutputStream writer = new FileOutputStream("res/numbers.txt");
+        //PrintWriter writer = new PrintWriter("res/numbers.txt");
 
-        char letters[] = {'У', 'К', 'Е', 'Н', 'Х', 'В', 'А', 'Р', 'О', 'С', 'М', 'Т'};
-        for(int number = 1; number < 1000; number++)
-        {
-            int regionCode = 199;
-            for (char firstLetter : letters)
-            {
-                for (char secondLetter : letters)
-                {
-                    for (char thirdLetter : letters)
-                    {
-                        String carNumber = firstLetter + padNumber(number, 3) +
-                            secondLetter + thirdLetter + padNumber(regionCode, 2);
-                        writer.write(carNumber.getBytes());
-                        writer.write('\n');
-                    }
-                }
-            }
-        }
 
-        writer.flush();
-        writer.close();
+        ForkJoinPool pool = new ForkJoinPool();
+        pool.invoke(new CarNumbers(1));
+        pool.shutdown();
 
         System.out.println((System.currentTimeMillis() - start) + " ms");
     }
 
-    private static String padNumber(int number, int numberLength)
-    {
-        String numberStr = Integer.toString(number);
-        int padSize = numberLength - numberStr.length();
-        for(int i = 0; i < padSize; i++) {
-            numberStr = '0' + numberStr;
-        }
-        return numberStr;
-    }
+
 }
